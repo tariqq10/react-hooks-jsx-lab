387a9ad2340a427ae752fdc0e2c021459e9412e9
KENT <kwilliam866@gmail.com>
2024-08-17 01:24:43 +0300

    jsx

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p> Many men, many, many, many men Wish death upon me Lord, I don't cry no more Don't look for me to cry no more Many men, many, many, many men Wish death upon me You wanna get rid of me But you can't get rid of me</p>
+    <img src={image} alt="I made this"></img>
+
+
+  </div>
+  );
 }
 
 export default About;
